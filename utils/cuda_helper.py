ef96b22d99464cfe0f89ff58b5a6e36cc512f627
Chujie Zheng <chujiezhengchn@gmail.com>
2020-10-12 10:44:55 +0800

    update

--- utils/cuda_helper.py
+++ utils/cuda_helper.py
@@ -23,20 +23,6 @@ def gpuones(*size):
     with torch.cuda.device(device_num):
         return torch.cuda.FloatTensor(*size).fill_(1)
 
-def check_mem(cuda_device):
-    devices_info = os.popen('"/usr/bin/nvidia-smi" --query-gpu=memory.total,memory.used --format=csv,nounits,noheader').read().strip().split("\n")
-    total, used = devices_info[int(cuda_device)].split(',')
-    return total,used
-
-def occumpy_mem(cuda_device):
-    total, used = check_mem(cuda_device)
-    total = int(total)
-    used = int(used)
-    max_mem = int(total * 0.9)
-    block_mem = max_mem - used
-    x = torch.cuda.FloatTensor(256,1024,block_mem)
-    del x
-
 # pylint: disable=W0603
 def init(device=None, __cuda=False):
     global _cuda, _zeros, _ones, use_cuda, device_num, _tensor_type, _long_tensor_type
